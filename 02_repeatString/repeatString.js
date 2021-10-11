86d5935e20ecff07e76921c8c0080ed7561feb82
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2021-10-12 00:00:02 +0545

    Add solution 01-07

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(msg, rept) {
+    if (rept < 0)
+        return "ERROR";
+    
+    let ret = "";
+    for (let i = 0; i < rept; ++i)
+        ret += msg;
+    return ret;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...rest) {
+    return arr.filter((val) => {
+        return !rest.includes(val);
+    })
 };
 
 // Do not edit below this line
